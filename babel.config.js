633491dda3a5bcc86015bc73a4f8ecf6365cbbc2
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-04-26 11:14:24 -0600

    installed plugin module resolver and added alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // ..other plugins if any
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
